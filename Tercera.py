33f36b4a20a6fffeeb7d69155ab7c46fed8167b0
Asus <emirand20>
2023-01-17 20:55:50 +0100

    acabadoSinPlot

--- Tercera.py
+++ Tercera.py
@@ -1,2 +1,9 @@
 import pandas as pd
+import datetime
 
+def fallecidos():
+    df = pd.read_csv("df_covid19_countries.csv", nrows=10)
+    a = df.loc[:,['reproduction_rate', 'date', 'location']]
+    #df['date'] = df['location'].dt.time
+    #df.groupby('date').mean()
+    return a
