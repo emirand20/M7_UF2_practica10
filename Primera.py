8b5b08e0f57ff68757869317ad5bc089aee2b306
Asus <emirand20>
2023-01-19 20:47:16 +0100

    se ha intentado marplotlib

--- Primera.py
+++ Primera.py
@@ -1,11 +1,9 @@
 import pandas as pd
 
-
 def pais():
     df = pd.read_csv("df_covid19_countries.csv", nrows=10000)
-    pais =  df.groupby(by='location').mean()
+    pais =  df.groupby(by='location').sum()
     a = pais.loc[:,['date', 'population']]
     return a
 #    a = df.loc[:,['location','date', 'population']]
-#    return a 
 print(pais())
--- Segunda.py
+++ Segunda.py
@@ -4,4 +4,5 @@ def morts():
     df = pd.read_csv("df_covid19_countries.csv", nrows=10)
     a = df.loc[:,['total_deaths','date','location']]
     return a
-    
+
+print(morts())
