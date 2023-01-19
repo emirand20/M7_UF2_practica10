5c53ee619df45187b78d166330f2115a31ec443b
Asus <emirand20>
2023-01-19 20:12:03 +0100

    returnPais

--- Primera.py
+++ Primera.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
-def pais():
-    df = pd.read_csv("df_covid19_countries.csv", nrows=10)
-    a = df.loc[:,['location','date', 'population']]
-    return a
 
-#print(pais())
+def pais():
+    df = pd.read_csv("df_covid19_countries.csv", nrows=10000)
+    pais =  df.groupby(by='location').mean()
+    a = pais.loc[:,['date', 'population']]
+    return a
+#    a = df.loc[:,['location','date', 'population']]
+#    return a 
+print(pais())
--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
 import Primera as a
 import Segunda as b
 import Tercera as c
 
+print("----------------------")
 print(a.pais())
+print("----------------------")
 print(b.morts())
+print("----------------------")
 print(c.fallecidos())
+
+def A():
+    gM = a.pais()
+
+    X = list(gM.iloc[:,0])
+    Y = list(gM.iloc[:, 1])
+
+    plt.plot(Y,X, color="red")
+    plt.title("Pais")
+    plt.xlabel("Id Pais")
+    plt.ylabel("Pais")
+    plt.show()
+
+A()
