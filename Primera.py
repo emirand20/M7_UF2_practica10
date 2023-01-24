42ef063b0f82918fd6c3eed1b7fb98e74c19684f
Asus <emirand20>
2023-01-24 20:23:16 +0100

    desistoA

--- Primera.py
+++ Primera.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 def pais():
     df = pd.read_csv("df_covid19_countries.csv", nrows=10000)
-    pais =  df.groupby(by='location').sum()
-    a = pais.loc[:,['date', 'population']]
+    a = df.loc[:, ['date', 'population']]
+    #a = df.loc[:['date', 'population']]
     return a
-#    a = df.loc[:,['location','date', 'population']]
+
 print(pais())
